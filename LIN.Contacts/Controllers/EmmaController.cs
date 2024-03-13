028f323725dd0fc981656691b21ffdb3b2b4b75d
Alex <giraldojhong4@gmail.com>
2024-03-13 16:09:40 -0500

    Mejoras del controlador de Emma

--- LIN.Contacts/Controllers/EmmaController.cs
+++ LIN.Contacts/Controllers/EmmaController.cs
@@ -1,5 +1,6 @@
 using LIN.Contacts.Memory;
 using LIN.Types.Emma.Models;
+using System.Text;
 
 namespace LIN.Contacts.Controllers;
 
@@ -7,6 +8,57 @@ namespace LIN.Contacts.Controllers;
 [Route("Emma")]
 public class EmmaController : ControllerBase
 {
+
+
+
+    /// <summary>
+    /// Consulta para LIN Allo Emma.
+    /// </summary>
+    /// <param name="token">Token de acceso.</param>
+    /// <param name="consult">Consulta.</param>
+    [HttpPost]
+    public async Task<HttpReadOneResponse<ResponseIAModel>> Assistant([FromHeader] string tokenAuth, [FromBody] string consult)
+    {
+
+
+
+        HttpClient client = new HttpClient();
+
+        client.DefaultRequestHeaders.Add("token", tokenAuth);
+        client.DefaultRequestHeaders.Add("useDefaultContext", true.ToString().ToLower());
+
+
+        var request = new LIN.Types.Models.EmmaRequest
+        {
+            AppContext = "contacts",
+            Asks = consult
+        };
+
+
+
+        StringContent stringContent = new(Newtonsoft.Json.JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+        var result = await client.PostAsync("http://api.emma.linapps.co/emma", stringContent);
+
+
+        var ss = await result.Content.ReadAsStringAsync();
+
+
+        dynamic fin = Newtonsoft.Json.JsonConvert.DeserializeObject(ss);
+
+
+        // Respuesta
+        return new ReadOneResponse<ResponseIAModel>()
+        {
+            Model = new()
+            {
+                IsSuccess = true,
+                Content = fin?.result
+            },
+            Response = Responses.Success
+        };
+
+    }
 
 
 
@@ -58,8 +110,38 @@ public class EmmaController : ControllerBase
         }
 
 
-        var final = getProf?.StringOfContacts() ?? "No hay contactos";
+        string final = $$""""
 
+                        Estos son los contactos que tiene el usuario:
+
+                        {{getProf?.StringOfContacts()}}
+
+                        Estos son comandos:
+                        
+                        {
+                          "name": "#say",
+                          "description": "Utiliza esta función para decirle algo al usuario como saludos o responder a preguntas.",
+                          "example":"#say('Hola')",
+                          "parameters": {
+                            "properties": {
+                              "content": {
+                                "type": "string",
+                                "description": "contenido"
+                              }
+                            }
+                          }
+                        }
+
+                        El formato para responder con comandos es:
+                        "#NombreDelComando(Propiedades en orden separados por coma si es necesario)"
+                        
+                        
+                        IMPORTANTE:
+                        No en todos los casos en necesario usar comandos, solo úsalos cuando se cumpla la descripción.
+
+                        NUNCA debes inventar comandos nuevos, solo puedes usar los que ya existen.
+
+                        """";
         return new ReadOneResponse<object>()
         {
             Model = final,
