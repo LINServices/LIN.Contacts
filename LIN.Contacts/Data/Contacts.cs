0ef6d653123820597307f1e4547e8e4b4ae011f9
Alex <giraldojhong4@gmail.com>
2024-03-23 21:31:06 -0500

    Mejoras

--- LIN.Contacts/Data/Contacts.cs
+++ LIN.Contacts/Data/Contacts.cs
@@ -9,139 +9,83 @@ public partial class Contacts
     /// Crear nuevo contacto.
     /// </summary>
     /// <param name="data">Modelo.</param>
-    /// <param name="context">Contexto.</param>
-    /// <returns></returns>
-    public static async Task<CreateResponse> Create(ContactModel data, Conexión context)
+    public static async Task<CreateResponse> Create(ContactModel data)
     {
-        // Ejecución
-        try
-        {
 
-            // Establecer el perfil.
-            foreach (var e in data.Mails)
-                e.Profile = data.Im;
+        // Contexto
+        (var context, var connectionKey) = Conexión.GetOneConnection();
 
-            // Establecer el perfil.
-            foreach (var e in data.Phones)
-                e.Profile = data.Im;
+        // respuesta
+        var response = await Create(data, context);
 
-            // El usuario ya existe.
-            context.DataBase.Attach(data.Im);
+        context.CloseActions(connectionKey);
 
-            var res = context.DataBase.Contacts.Add(data);
-            await context.DataBase.SaveChangesAsync();
-            return new(Responses.Success, data.Id);
-        }
-        catch
-        {
-        }
-        return new();
+        return response;
+
     }
 
 
 
     /// <summary>
-    /// Obtiene un contacto.
+    /// Obtener un contacto.
     /// </summary>
-    /// <param name="id">Id del contacto</param>
-    /// <param name="context">Contexto de conexión.</param>
-    public static async Task<ReadOneResponse<ContactModel>> Read(int id, Conexión context)
+    /// <param name="id">Id del contacto.</param>
+    public static async Task<ReadOneResponse<ContactModel>> Read(int id)
     {
 
-        // Ejecución
-        try
-        {
+        // Contexto
+        (var context, var connectionKey) = Conexión.GetOneConnection();
 
-            var profile = await (from P in context.DataBase.Contacts
-                                 where P.Id == id
-                                 select new ContactModel
-                                 {
-                                     Picture = P.Picture,
-                                     Birthday = P.Birthday,
-                                     Id = P.Id,
-                                     Mails = P.Mails,
-                                     Nombre = P.Nombre,
-                                     Type = P.Type,
-                                     Phones = P.Phones
-                                 }).FirstOrDefaultAsync();
+        // respuesta
+        var response = await Read(id, context);
 
-            return new(Responses.Success, profile ?? new());
-        }
-        catch
-        {
-        }
-        return new();
+        context.CloseActions(connectionKey);
+
+        return response;
+
     }
 
 
 
     /// <summary>
-    /// Acceso IAM en un contacto.
+    /// Validar acceso Iam.
     /// </summary>
     /// <param name="contact">Id del contacto.</param>
     /// <param name="profile">Id del perfil.</param>
-    /// <param name="context">Contexto de conexión.</param>
-    public static async Task<ReadOneResponse<bool>> Iam(int contact, int profile, Conexión context)
+    public static async Task<ReadOneResponse<bool>> Iam(int contact, int profile)
     {
 
-        // Ejecución
-        try
-        {
+        // Contexto
+        (var context, var connectionKey) = Conexión.GetOneConnection();
 
-            var have = await (from P in context.DataBase.Contacts
-                              where P.Id == contact
-                              && P.Im.Id == profile
-                              select P.Id
-                                 ).FirstOrDefaultAsync();
+        // respuesta
+        var response = await Iam(contact, profile, context);
 
+        context.CloseActions(connectionKey);
 
-            if (have <= 0)
-                return new(Responses.Unauthorized);
+        return response;
 
-            return new(Responses.Success, true);
-        }
-        catch
-        {
-        }
-        return new();
     }
 
 
 
     /// <summary>
-    /// Obtiene los contactos asociados a un perfil.
+    /// Obtener los contactos asociados a un perfil.
     /// </summary>
-    /// <param name="id">Id del perfil.</param>
-    /// <param name="context">Contexto de conexión.</param>
-    public static async Task<ReadAllResponse<ContactModel>> ReadAll(int id, Conexión context)
+    /// <param name="id"></param>
+    public static async Task<ReadAllResponse<ContactModel>> ReadAll(int id)
     {
 
+        // Contexto
+        (var context, var connectionKey) = Conexión.GetOneConnection();
 
-        // Ejecución
-        try
-        {
+        // respuesta
+        var response = await ReadAll(id, context);
 
-            // Query de contactos
-            var contacts = await (from contact in context.DataBase.Contacts
-                                  where contact.Im.Id == id
-                                  orderby contact.Nombre
-                                  select new ContactModel
-                                  {
-                                      Picture = contact.Picture,
-                                      Birthday = contact.Birthday,
-                                      Id = contact.Id,
-                                      Mails = contact.Mails,
-                                      Nombre = contact.Nombre,
-                                      Type = contact.Type,
-                                      Phones = contact.Phones
-                                  }).ToListAsync();
+        context.CloseActions(connectionKey);
 
-            return new(Responses.Success, contacts);
-        }
-        catch
-        {
-        }
-        return new();
+        return response;
+
     }
 
 
@@ -150,23 +94,19 @@ public partial class Contacts
     /// Eliminar un contacto.
     /// </summary>
     /// <param name="id">Id del contacto.</param>
-    /// <param name="context">Contexto de conexión.</param>
-    public static async Task<ResponseBase> Delete(int id, Conexión context)
+    public static async Task<ResponseBase> Delete(int id)
     {
 
-        // Ejecución
-        try
-        {
+        // Contexto
+        (var context, var connectionKey) = Conexión.GetOneConnection();
 
-            // Eliminar.
-            await context.DataBase.Contacts.Where(t => t.Id == id).ExecuteDeleteAsync();
+        // respuesta
+        var response = await Delete(id, context);
 
-            return new(Responses.Success);
-        }
-        catch
-        {
-        }
-        return new();
+        context.CloseActions(connectionKey);
+
+        return response;
+
     }
 
 
@@ -174,51 +114,21 @@ public partial class Contacts
     /// <summary>
     /// Actualizar un contacto.
     /// </summary>
-    /// <param name="id">Id del contacto.</param>
-    /// <param name="contactModel">Modelo del contacto.</param>
-    /// <param name="context">Contexto.</param>
-    public static async Task<ResponseBase> Update(ContactModel contactModel, Conexión context)
+    /// <param name="contactModel">Modelo</param>
+    public static async Task<ResponseBase> Update(ContactModel contactModel)
     {
 
-        // Ejecución
-        try
-        {
+        // Contexto
+        (var context, var connectionKey) = Conexión.GetOneConnection();
 
-            // Eliminar.
-            await context.DataBase.Contacts.Where(t => t.Id == contactModel.Id).ExecuteUpdateAsync(
-                setters =>
-                    setters.SetProperty(contact => contact.Picture, b => contactModel.Picture ?? b.Picture).
-                            SetProperty(contact => contact.Nombre, b => contactModel.Nombre ?? b.Nombre)
-                );
+        // respuesta
+        var response = await Update(contactModel, context);
 
+        context.CloseActions(connectionKey);
 
-            // Actualizar números.
-            foreach (var phone in contactModel.Phones)
-            {
-                await context.DataBase.Phones.Where(t => t.Id == phone.Id).ExecuteUpdateAsync(
-                setters =>
-                    setters.SetProperty(contact => contact.Number, b => phone.Number ?? b.Number)
-                );
-            }
+        return response;
 
-
-            // Actualizar correos.
-            foreach (var mails in contactModel.Mails)
-            {
-                await context.DataBase.Mails.Where(t => t.Id == mails.Id).ExecuteUpdateAsync(
-                setters =>
-                    setters.SetProperty(contact => contact.Email, b => mails.Email ?? b.Email)
-                );
-            }
-
-            return new(Responses.Success);
-        }
-        catch
-        {
-        }
-        return new();
     }
 
-
-
+   
 }
--- LIN.Contacts/Data/Contacts.cs.cs
+++ LIN.Contacts/Data/Contacts.cs.cs
@@ -4,90 +4,143 @@
 public partial class Contacts
 {
 
-  
 
     /// <summary>
     /// Crear nuevo contacto.
     /// </summary>
     /// <param name="data">Modelo.</param>
-    public static async Task<CreateResponse> Create(ContactModel data)
+    /// <param name="context">Contexto.</param>
+    public static async Task<CreateResponse> Create(ContactModel data, Conexión context)
     {
+        // Ejecución
+        try
+        {
 
-        // Contexto
-        (var context, var connectionKey) = Conexión.GetOneConnection();
+            // Establecer el perfil.
+            foreach (var e in data.Mails)
+                e.Profile = data.Im;
 
-        // respuesta
-        var response = await Create(data, context);
+            // Establecer el perfil.
+            foreach (var e in data.Phones)
+                e.Profile = data.Im;
 
-        context.CloseActions(connectionKey);
+            // El usuario ya existe.
+            context.DataBase.Attach(data.Im);
 
-        return response;
-
+            var res = context.DataBase.Contacts.Add(data);
+            await context.DataBase.SaveChangesAsync();
+            return new(Responses.Success, data.Id);
+        }
+        catch
+        {
+        }
+        return new();
     }
 
 
 
     /// <summary>
-    /// Obtener un contacto.
+    /// Obtiene un contacto.
     /// </summary>
-    /// <param name="id">Id del contacto.</param>
-    public static async Task<ReadOneResponse<ContactModel>> Read(int id)
+    /// <param name="id">Id del contacto</param>
+    /// <param name="context">Contexto de conexión.</param>
+    public static async Task<ReadOneResponse<ContactModel>> Read(int id, Conexión context)
     {
 
-        // Contexto
-        (var context, var connectionKey) = Conexión.GetOneConnection();
+        // Ejecución
+        try
+        {
 
-        // respuesta
-        var response = await Read(id, context);
+            var profile = await (from P in context.DataBase.Contacts
+                                 where P.Id == id
+                                 select new ContactModel
+                                 {
+                                     Picture = P.Picture,
+                                     Birthday = P.Birthday,
+                                     Id = P.Id,
+                                     Mails = P.Mails,
+                                     Nombre = P.Nombre,
+                                     Type = P.Type,
+                                     Phones = P.Phones
+                                 }).FirstOrDefaultAsync();
 
-        context.CloseActions(connectionKey);
-
-        return response;
-
+            return new(Responses.Success, profile ?? new());
+        }
+        catch
+        {
+        }
+        return new();
     }
 
 
 
     /// <summary>
-    /// Validar acceso Iam.
+    /// Acceso IAM en un contacto.
     /// </summary>
     /// <param name="contact">Id del contacto.</param>
     /// <param name="profile">Id del perfil.</param>
-    public static async Task<ReadOneResponse<bool>> Iam(int contact, int profile)
+    /// <param name="context">Contexto de conexión.</param>
+    public static async Task<ReadOneResponse<bool>> Iam(int contact, int profile, Conexión context)
     {
 
-        // Contexto
-        (var context, var connectionKey) = Conexión.GetOneConnection();
+        // Ejecución
+        try
+        {
 
-        // respuesta
-        var response = await Iam(contact, profile, context);
+            var have = await (from P in context.DataBase.Contacts
+                              where P.Id == contact
+                              && P.Im.Id == profile
+                              select P.Id
+                                 ).FirstOrDefaultAsync();
 
-        context.CloseActions(connectionKey);
 
-        return response;
+            if (have <= 0)
+                return new(Responses.Unauthorized);
 
+            return new(Responses.Success, true);
+        }
+        catch
+        {
+        }
+        return new();
     }
 
 
 
     /// <summary>
-    /// Obtener los contactos asociados a un perfil.
+    /// Obtiene los contactos asociados a un perfil.
     /// </summary>
-    /// <param name="id"></param>
-    /// <returns></returns>
-    public static async Task<ReadAllResponse<ContactModel>> ReadAll(int id)
+    /// <param name="id">Id del perfil.</param>
+    /// <param name="context">Contexto de conexión.</param>
+    public static async Task<ReadAllResponse<ContactModel>> ReadAll(int id, Conexión context)
     {
 
-        // Contexto
-        (var context, var connectionKey) = Conexión.GetOneConnection();
 
-        // respuesta
-        var response = await ReadAll(id, context);
+        // Ejecución
+        try
+        {
 
-        context.CloseActions(connectionKey);
+            // Query de contactos
+            var contacts = await (from contact in context.DataBase.Contacts
+                                  where contact.Im.Id == id
+                                  orderby contact.Nombre
+                                  select new ContactModel
+                                  {
+                                      Picture = contact.Picture,
+                                      Birthday = contact.Birthday,
+                                      Id = contact.Id,
+                                      Mails = contact.Mails,
+                                      Nombre = contact.Nombre,
+                                      Type = contact.Type,
+                                      Phones = contact.Phones
+                                  }).ToListAsync();
 
-        return response;
-
+            return new(Responses.Success, contacts);
+        }
+        catch
+        {
+        }
+        return new();
     }
 
 
@@ -95,20 +148,24 @@ public partial class Contacts
     /// <summary>
     /// Eliminar un contacto.
     /// </summary>
-    /// <param contact="id">Id del contacto.</param>
-    public static async Task<ResponseBase> Delete(int id)
+    /// <param name="id">Id del contacto.</param>
+    /// <param name="context">Contexto de conexión.</param>
+    public static async Task<ResponseBase> Delete(int id, Conexión context)
     {
 
-        // Contexto
-        (var context, var connectionKey) = Conexión.GetOneConnection();
+        // Ejecución
+        try
+        {
 
-        // respuesta
-        var response = await Delete(id, context);
+            // Eliminar.
+            await context.DataBase.Contacts.Where(t => t.Id == id).ExecuteDeleteAsync();
 
-        context.CloseActions(connectionKey);
-
-        return response;
-
+            return new(Responses.Success);
+        }
+        catch
+        {
+        }
+        return new();
     }
 
 
@@ -116,21 +173,51 @@ public partial class Contacts
     /// <summary>
     /// Actualizar un contacto.
     /// </summary>
-    /// <param contact="id">Id del contacto.</param>
-    public static async Task<ResponseBase> Update(ContactModel contactModel)
+    /// <param name="id">Id del contacto.</param>
+    /// <param name="contactModel">Modelo del contacto.</param>
+    /// <param name="context">Contexto.</param>
+    public static async Task<ResponseBase> Update(ContactModel contactModel, Conexión context)
     {
 
-        // Contexto
-        (var context, var connectionKey) = Conexión.GetOneConnection();
+        // Ejecución
+        try
+        {
 
-        // respuesta
-        var response = await Update(contactModel, context);
+            // Eliminar.
+            await context.DataBase.Contacts.Where(t => t.Id == contactModel.Id).ExecuteUpdateAsync(
+                setters =>
+                    setters.SetProperty(contact => contact.Picture, b => contactModel.Picture ?? b.Picture).
+                            SetProperty(contact => contact.Nombre, b => contactModel.Nombre ?? b.Nombre)
+                );
 
-        context.CloseActions(connectionKey);
 
-        return response;
+            // Actualizar números.
+            foreach (var phone in contactModel.Phones)
+            {
+                await context.DataBase.Phones.Where(t => t.Id == phone.Id).ExecuteUpdateAsync(
+                setters =>
+                    setters.SetProperty(contact => contact.Number, b => phone.Number ?? b.Number)
+                );
+            }
 
+
+            // Actualizar correos.
+            foreach (var mails in contactModel.Mails)
+            {
+                await context.DataBase.Mails.Where(t => t.Id == mails.Id).ExecuteUpdateAsync(
+                setters =>
+                    setters.SetProperty(contact => contact.Email, b => mails.Email ?? b.Email)
+                );
+            }
+
+            return new(Responses.Success);
+        }
+        catch
+        {
+        }
+        return new();
     }
+
 
 
 }
