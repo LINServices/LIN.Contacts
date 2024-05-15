3c56fb5a8764c3d9b58a903e1763cab93eb70fd7
Alex <giraldojhong4@gmail.com>
2024-05-15 08:56:37 -0500

    Mejoras en con HTTP LIN

--- LIN.Contacts/Program.cs
+++ LIN.Contacts/Program.cs
@@ -1,18 +1,7 @@
+using Http.Extensions;
 
 // Crear constructor.
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-// CORS.
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAnyOrigin",
-        builder =>
-        {
-            builder.AllowAnyOrigin()
-               .AllowAnyHeader()
-               .AllowAnyMethod();
-        });
-});
 
 // Obtiene el string de conexión SQL.
 var sqlConnection = builder.Configuration["ConnectionStrings:somee"] ?? string.Empty;
@@ -25,9 +14,8 @@ builder.Services.AddDbContext<Context>(options =>
 
 
 // Controladores.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddLINHttp();
+
 builder.Services.AddSignalR();
 
 // Crear app.
@@ -44,20 +32,8 @@ catch (Exception)
 {
 }
 
-// Usar CORS.
-app.UseCors("AllowAnyOrigin");
 
-// Swagger.
-app.UseSwagger();
-app.UseSwaggerUI(config =>
-{
-    config.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
-    config.RoutePrefix = string.Empty;
-});
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseLINHttp();
 
 // Establecer string de conexión.
 Conexión.SetStringConnection(sqlConnection);
